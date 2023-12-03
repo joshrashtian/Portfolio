4f22d3a11ca1444a36b2144d2ec86befbdf7ea57
Joshua Rashtian <joshrashtian1@gmail.com>
2023-12-02 22:49:46 -0800

    GitHub Link in Project Data

--- src/components/ProjectData.jsx
+++ src/components/ProjectData.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
+import GitHub from "../assets/projects/github.png"
+import Globe from "../assets/projects/globe.png"
 
 export default function ProjectData(project) {
   return (
@@ -13,13 +15,13 @@ export default function ProjectData(project) {
         src={project.project.logo}
         className=" h-[30%] w-[30%] mr-7 rounded-3xl shadow-lg transition-all duration-500"
         initial={{ scale: 0.8, opacity: "0%" }}
-        whileHover={{scale: 1.1}}
+        whileHover={{ scale: 1.1 }}
         animate={{ scale: 1, opacity: "100%" }}
-         transition={{
-            duration: 0.5,
-            type: "spring",
-            damping: 25,
-            stiffness: 500,
+        transition={{
+          duration: 0.5,
+          type: "spring",
+          damping: 25,
+          stiffness: 500,
         }}
       />
       <div>
@@ -44,7 +46,10 @@ export default function ProjectData(project) {
         <h2>{project.project.description}</h2>
         <div className="flex flex-wrap">
           {project.project.tools.map((tool, index) => (
-            <h1 key={index} className="p-1 px-3 rounded-lg bg-slate-300 shadow-sm m-1 mt-3 text-teal-950 font-semibold ">
+            <h1
+              key={index}
+              className="p-1 px-3 rounded-lg bg-slate-300 shadow-sm m-1 mt-3 text-teal-950 font-semibold "
+            >
               {tool}
             </h1>
           ))}
@@ -58,8 +63,9 @@ export default function ProjectData(project) {
               className="flex bg-slate-400 my-0.5"
               initial={{ x: -20, opacity: "0%" }}
               animate={{ x: 0, opacity: "100%" }}
+              key={index}
               transition={{
-                delay: 0.5 + (index * 0.5),
+                delay: 0.5 + index * 0.5,
                 duration: 0.5,
                 type: "spring",
                 damping: 25,
@@ -79,6 +85,51 @@ export default function ProjectData(project) {
             </motion.div>
           ))}
         </div>
+        <motion.div
+        initial={{ x: -20, opacity: "0%" }}
+        animate={{ x: 0, opacity: "100%" }}
+        transition={{
+          delay: 1.5,
+          duration: 0.5,
+          type: "spring",
+          damping: 25,
+          stiffness: 500,
+        }} >
+        {project.project.source_code != undefined ? (
+          <div className=" w-52 h-9 flex bg-slate-800 items-center justify-center hover:justify-evenly rounded-[10px] hover:rounded-[6px] hover:scale-[1.1] hover:bg-slate-600 transition-all duration-400 shadow-sm my-2 group">
+            <img
+              src={GitHub}
+              className="w-6 h-6 rounded-lg group-hover:scale-100 scale-0 transition-all duration-400"
+            />
+            <a
+              className="font-bold align-middle text-white transition-all duration-400"
+              href={
+                project.project.source_code != undefined
+                  ? project.project.source_code
+                  : null
+              }
+            >
+              {project.project.source_code != undefined
+                ? "View Source Code"
+                : null}
+            </a>
+          </div>
+        ) : null}
+        {project.project.website != undefined ? (
+          <div className=" w-52 h-10 items-center text-center justify-center hover:justify-evenly flex bg-teal-700 rounded-[20px] hover:rounded-[10px] hover:scale-[1.1] hover:bg-teal-800 transition-all duration-400 shadow-sm my-2 group">
+            <img
+              src={Globe}
+              className="w-5 h-5 rounded-lg group-hover:scale-100 scale-0 transition-all duration-400"
+            />
+            <a
+              className="font-bold align-middle group-hover:mr-3 text-white transition-all duration-300"
+              href={project.project.website != undefined ? project.project.website : null}
+            >
+              Visit Website
+            </a>
+          </div>
+        ) : null}
+        </motion.div>
       </div>
     </motion.div>
   );
