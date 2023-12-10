4fa4aad5dd048850804fa3295a71b643bf0af48e
Joshua Rashtian <joshrashtian1@gmail.com>
2023-12-09 23:46:40 -0800

    Revert "React Router Fix"
    This reverts commit 34e66c21ca5f4ec53a6aa41b667f11720a843d3b.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    input: [
+      'src/main.jsx',
+      'src/index.css',
+      'src/App.jsx',
+      'src/App.css'
+    ],
+    refresh: true,
+  }),
+],
   build: { chunkSizeWarningLimit: 1600, }
 })
